42f6d022b761d7777ccf66fe0ba0aded135675c6
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-11 22:28:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -416,11 +416,13 @@ async def start(client, message):
             if STREAM_MODE:
                 btn = [
                     [InlineKeyboardButton('𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝖾𝗆𝗂𝗇𝗀 𝖫𝗂𝗇𝗄', callback_data=f'streamfile:{file_id}')],
-                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]  
+                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+                    [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]
                 ]
             else:
                 btn = [
-                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]
+                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+                    [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]
                 ]
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -455,12 +457,13 @@ async def start(client, message):
             if STREAM_MODE:
                 btn = [
                     [InlineKeyboardButton('𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝖾𝗆𝗂𝗇𝗀 𝖫𝗂𝗇𝗄', callback_data=f'streamfile:{file_id}')],
-                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]
-             
+                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+                    [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]           
                 ]
             else:
                 btn = [
-                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]
+                    [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+                    [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]
                 ]
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -513,11 +516,13 @@ async def start(client, message):
     if STREAM_MODE:
         btn = [
             [InlineKeyboardButton('𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝖾𝗆𝗂𝗇𝗀 𝖫𝗂𝗇𝗄', callback_data=f'streamfile:{file_id}')],
-            [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]
+            [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+            [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]
         ]
     else:
         btn = [
-            [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)]
+            [InlineKeyboardButton('𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅', url=UPDATE_CHANNEL_LNK)],
+            [InlineKeyboardButton("📊 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 𝗜𝗻𝗳𝗼", callback_data=f"fileinfo#{file_id}")]
         ]
     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
